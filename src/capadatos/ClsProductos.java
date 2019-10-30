77bf1d2939b905520316f67196d95acac18b30e6
Yajaira Cuatis <56648539+Yaja6@users.noreply.github.com>
2019-10-30 17:52:56 -0500

    Agregué la función "leer productos" en donde el resultset "producto" va a obtener los valores de la base de datos según las variables que utilicé Estas variables les puse similiares a las que tenemos en la tabla de productos para no confundirnos

--- src/capadatos/ClsProductos.java
+++ src/capadatos/ClsProductos.java
@@ -1,5 +1,8 @@
 package capadatos;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ClsProductos {
 
     private int _claveProducto;
@@ -15,6 +18,14 @@ public class ClsProductos {
         _cantProducto = 0;
         _uniProducto = null;
 
+    }
+    
+    public void leerProducto(ResultSet producto) throws SQLException{
+        _claveProducto = producto.getInt("claveProducto");
+        _nomProducto = producto.getString("nomProducto");
+        _precioProducto = producto.getString("precioProducto");
+        _cantProducto = producto.getInt("cantProducto");
+        _uniProducto = producto.getString("unidProducto");       
     }
 
 }
