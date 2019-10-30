421d1d92d4f6e05cdd043aeff7e2cbba3949d9a2
Yajaira Cuatis <56648539+Yaja6@users.noreply.github.com>
2019-10-30 17:42:31 -0500

    Variables para el registro de Productos

---
+++ src/capadatos/ClsProductos.java
@@ -0,0 +1,11 @@
+package capadatos;
+
+public class ClsProductos {
+
+    private int _claveProducto;
+    private String _nomProducto;
+    private String _precioProducto;
+    private int _cantProducto;
+    private String _uniProducto;
+
+}
