5948fd621e948865ebb3c005372ffc2228385b5c
Yajaira Cuatis <56648539+Yaja6@users.noreply.github.com>
2019-10-30 19:06:18 -0500

    Agregué un nuevo panel para el item "Nuevo Poducto" solo me faltaría programar el boton que me guarde el producto nuevo a la BD

--- src/capainteraz/WinPrincipal.java
+++ src/capainteraz/WinPrincipal.java
@@ -19,13 +19,14 @@ import javax.swing.table.DefaultTableModel;
  */
 public class WinPrincipal extends javax.swing.JFrame {
 
-   Clsconexion _vendedor, _cliente, _producto, _factura, _delfactura;
+    Clsconexion _vendedor, _cliente, _producto, _factura, _delfactura;
     ResultSet rsclientes;
     int clv;
+
     public WinPrincipal() {
         initComponents();
-         archbd.setVisible(false);
-         PnlVerProductos.setVisible(false);
+        archbd.setVisible(false);
+        PnlVerProductos.setVisible(false);
     }
 
     /**
@@ -37,6 +38,7 @@ public class WinPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel5 = new javax.swing.JPanel();
         archbd = new javax.swing.JFileChooser();
         PnlVerProductos = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
@@ -45,6 +47,21 @@ public class WinPrincipal extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tablaProd = new javax.swing.JTable();
         BtnGenerar = new javax.swing.JButton();
+        PnlNuevoProducto = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel4 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        txtnomP = new javax.swing.JTextField();
+        txtprecio = new javax.swing.JTextField();
+        txtcantidad = new javax.swing.JTextField();
+        txtunidad = new javax.swing.JTextField();
+        BtnGuardarProd = new javax.swing.JButton();
+        lblmensaj = new javax.swing.JLabel();
+        jPanel6 = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -52,6 +69,17 @@ public class WinPrincipal extends javax.swing.JFrame {
         jMenu2 = new javax.swing.JMenu();
         jMenuItem3 = new javax.swing.JMenuItem();
         jMenuItem4 = new javax.swing.JMenuItem();
+
+        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
+        jPanel5.setLayout(jPanel5Layout);
+        jPanel5Layout.setHorizontalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel5Layout.setVerticalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(102, 102, 102));
@@ -150,6 +178,153 @@ public class WinPrincipal extends javax.swing.JFrame {
 
         getContentPane().add(PnlVerProductos, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 620, 360));
 
+        PnlNuevoProducto.setBackground(new java.awt.Color(102, 102, 102));
+
+        jPanel3.setBackground(new java.awt.Color(204, 204, 255));
+        jPanel3.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        jLabel2.setFont(new java.awt.Font("Open Sans", 0, 18)); // NOI18N
+        jLabel2.setText("NUEVO PRODUCTO");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(124, 124, 124))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel4.setBackground(new java.awt.Color(204, 204, 255));
+        jPanel4.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        jLabel3.setFont(new java.awt.Font("Open Sans", 0, 14)); // NOI18N
+        jLabel3.setText("Nombre Producto:");
+
+        jLabel4.setFont(new java.awt.Font("Open Sans", 0, 14)); // NOI18N
+        jLabel4.setText("Precio:");
+
+        jLabel5.setFont(new java.awt.Font("Open Sans", 0, 14)); // NOI18N
+        jLabel5.setText("Cantidad:");
+
+        jLabel6.setFont(new java.awt.Font("Open Sans", 0, 14)); // NOI18N
+        jLabel6.setText("Unidad(caja/unidad(es)):");
+
+        txtnomP.setFont(new java.awt.Font("Open Sans", 0, 14)); // NOI18N
+
+        txtprecio.setFont(new java.awt.Font("Open Sans", 0, 14)); // NOI18N
+
+        txtcantidad.setFont(new java.awt.Font("Open Sans", 0, 14)); // NOI18N
+
+        txtunidad.setFont(new java.awt.Font("Open Sans", 0, 14)); // NOI18N
+
+        BtnGuardarProd.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Save-icon.png"))); // NOI18N
+        BtnGuardarProd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnGuardarProdActionPerformed(evt);
+            }
+        });
+
+        lblmensaj.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        lblmensaj.setForeground(new java.awt.Color(255, 0, 0));
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(BtnGuardarProd, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(262, 262, 262))
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel4Layout.createSequentialGroup()
+                        .addGap(68, 68, 68)
+                        .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel3))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtnomP, javax.swing.GroupLayout.PREFERRED_SIZE, 265, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addComponent(txtunidad, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 141, Short.MAX_VALUE)
+                                .addComponent(txtcantidad, javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(txtprecio, javax.swing.GroupLayout.Alignment.LEADING))))
+                    .addGroup(jPanel4Layout.createSequentialGroup()
+                        .addGap(195, 195, 195)
+                        .addComponent(lblmensaj, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(61, Short.MAX_VALUE))
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtnomP, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(txtprecio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(txtcantidad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28)
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(txtunidad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(BtnGuardarProd, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblmensaj, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(30, Short.MAX_VALUE))
+        );
+
+        jPanel6.setBackground(new java.awt.Color(204, 204, 255));
+        jPanel6.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
+        jPanel6.setLayout(jPanel6Layout);
+        jPanel6Layout.setHorizontalGroup(
+            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanel6Layout.setVerticalGroup(
+            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 56, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout PnlNuevoProductoLayout = new javax.swing.GroupLayout(PnlNuevoProducto);
+        PnlNuevoProducto.setLayout(PnlNuevoProductoLayout);
+        PnlNuevoProductoLayout.setHorizontalGroup(
+            PnlNuevoProductoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        PnlNuevoProductoLayout.setVerticalGroup(
+            PnlNuevoProductoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(PnlNuevoProductoLayout.createSequentialGroup()
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        getContentPane().add(PnlNuevoProducto, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 620, 370));
+
         jMenu1.setText("Base de datos");
 
         jMenuItem1.setText("abrir ");
@@ -208,8 +383,8 @@ public class WinPrincipal extends javax.swing.JFrame {
 
     private void archbdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_archbdActionPerformed
 
-         File f = archbd.getSelectedFile();
-       String basededatos = f.getAbsolutePath();
+        File f = archbd.getSelectedFile();
+        String basededatos = f.getAbsolutePath();
         _vendedor = new Clsconexion(basededatos);
         _cliente = new Clsconexion(basededatos);
         _producto = new Clsconexion(basededatos);
@@ -224,7 +399,7 @@ public class WinPrincipal extends javax.swing.JFrame {
         } catch (SQLException ex) {
             Logger.getLogger(WinPrincipal.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         // TODO add your handling code here:
     }//GEN-LAST:event_archbdActionPerformed
 
@@ -233,35 +408,39 @@ public class WinPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem4ActionPerformed
 
     private void BtnGenerarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnGenerarActionPerformed
-        
-        DefaultTableModel modelo=(DefaultTableModel)tablaProd.getModel();
-        
-        String []c=new String [7];
-        
-        try{
-            for(int i=0;i<8 ;i++){
+
+        DefaultTableModel modelo = (DefaultTableModel) tablaProd.getModel();
+
+        String[] c = new String[7];
+
+        try {
+            for (int i = 0; i < 8; i++) {
                 _producto.siguiente();
-                c[0]=String.valueOf(_producto._ventas().getInt("claveProducto"));
-                c[1]=_producto._ventas().getString("nomProducto");
-                c[2]=_producto._ventas().getString("precioProducto");
-                c[3]=String.valueOf(_producto._ventas().getInt("cantProducto"));
-                c[4]=_producto._ventas().getString("unidProducto");
-                
+                c[0] = String.valueOf(_producto._ventas().getInt("claveProducto"));
+                c[1] = _producto._ventas().getString("nomProducto");
+                c[2] = _producto._ventas().getString("precioProducto");
+                c[3] = String.valueOf(_producto._ventas().getInt("cantProducto"));
+                c[4] = _producto._ventas().getString("unidProducto");
+
                 modelo.addRow(c);
             }
         } catch (SQLException ex) {
-           Logger.getLogger(WinPrincipal.class.getName()).log(Level.SEVERE, null, ex);
-       }
-        
+            Logger.getLogger(WinPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         tablaProd.setModel(modelo);
-        
+
     }//GEN-LAST:event_BtnGenerarActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
         archbd.setVisible(false);
         PnlVerProductos.setVisible(true);
-        
+
     }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+    private void BtnGuardarProdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnGuardarProdActionPerformed
+        
+    }//GEN-LAST:event_BtnGuardarProdActionPerformed
 
     /**
      * @param args the command line arguments
@@ -300,9 +479,16 @@ public class WinPrincipal extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BtnGenerar;
+    private javax.swing.JButton BtnGuardarProd;
+    private javax.swing.JPanel PnlNuevoProducto;
     private javax.swing.JPanel PnlVerProductos;
     private javax.swing.JFileChooser archbd;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
@@ -312,7 +498,16 @@ public class WinPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
+    private javax.swing.JPanel jPanel6;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblmensaj;
     private javax.swing.JTable tablaProd;
+    private javax.swing.JTextField txtcantidad;
+    private javax.swing.JTextField txtnomP;
+    private javax.swing.JTextField txtprecio;
+    private javax.swing.JTextField txtunidad;
     // End of variables declaration//GEN-END:variables
 }
